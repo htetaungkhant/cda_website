7e3dacf109dd0c51b794717fba78571f54527635
Htet Aung Khant <chitkogyi19950@gmail.com>
2025-07-15 16:26:18 +0700

    feat: implement Header component with responsive navigation and dropdown functionality

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,39 +1,193 @@
+"use client";
+import { useState } from "react";
 import Image from "next/image";
 import Link from "next/link";
+import { FaArrowRightLong } from "react-icons/fa6";
+import { RxHamburgerMenu, RxCross2 } from "react-icons/rx";
 
 import Logo from "@/public/logo.png";
 import { ButtonStyle1 } from "./Button";
+import HeaderDropdown from "./HeaderDropdown";
 
 const Header = () => {
+  const [hamburgerDisplay, setHamburgerDisplay] = useState(false);
+
+  const handleHamburgerClick = () => {
+    setHamburgerDisplay(!hamburgerDisplay);
+  };
+
   return (
-    <header className="fixed top-0 left-0 right-0 p-8">
-      <div className="flex justify-between items-center px-4 py-2 bg-[#FFFFFFB2] border border-[#00000033] rounded-4xl">
-        <div>
+    <header className="fixed top-0 left-0 right-0 lg:p-8">
+      <div className="max-lg:relative max-lg:z-20 flex justify-between items-center px-4 py-2 bg-[#FFFFFFB2] border border-[#00000033] rounded-4xl max-lg:rounded-none max-lg:border-none max-lg:bg-white">
+        <Link href="/" className="flex items-center">
           <Image
             src={Logo}
             width={248}
             height={76}
             alt="Logo"
-            className="w-48 h-16 object-contain"
+            className="w-48 h-16 max-lg:w-36 max-lg:h-12 object-contain"
           />
-        </div>
-        <ul className="flex gap-6 font-semibold">
-          <li>
-            <Link href="/">ABOUT US</Link>
-          </li>
-          <li>
-            <Link href="/about">OUR TEAM</Link>
-          </li>
-          <li>
-            <Link href="/about">EXPLORE CLASSES</Link>
-          </li>
-          <li>
-            <Link href="/about">CONTACT US</Link>
-          </li>
-        </ul>
-        <div>
+        </Link>
+        <button
+          onClick={handleHamburgerClick}
+          className="relative lg:hidden text-black bg-[var(--custom-primary)] outline-none ring-0 rounded-lg w-8 h-8 cursor-pointer transition"
+        >
+          <RxCross2
+            className={`absolute top-0 left-0 w-full h-full p-1 duration-300 ${
+              hamburgerDisplay ? "opacity-100 rotate-90" : "opacity-0"
+            }`}
+          />
+          <RxHamburgerMenu
+            className={`absolute top-0 left-0 w-full h-full p-1 duration-300 ${
+              hamburgerDisplay ? "opacity-0" : "opacity-100"
+            }`}
+          />
+        </button>
+        <nav className="max-lg:hidden">
+          <ul className="flex gap-8 text-sm font-semibold">
+            <li>
+              <Link href="/">ABOUT US</Link>
+            </li>
+            <li>
+              <Link href="/">OUR TEAM</Link>
+            </li>
+            <li>
+              <HeaderDropdown
+                title="EXPLORE CLASSES"
+                titleHref="/explore-classes"
+              >
+                <ul className="px-2 py-2 text-xs font-medium">
+                  <li className="flex items-center rounded-md text-black hover:bg-gray-100 cursor-pointer">
+                    <Link
+                      href="/classes/1"
+                      className="flex items-center justify-between w-full px-2 py-2 whitespace-nowrap"
+                    >
+                      MANUAL DRIVING CLASS
+                      <FaArrowRightLong className="w-4 h-4" />
+                    </Link>
+                  </li>
+                  <li className="flex rounded-md text-black hover:bg-gray-100 cursor-pointer">
+                    <Link
+                      href="/classes/2"
+                      className="flex items-center justify-between w-full px-2 py-2 whitespace-nowrap"
+                    >
+                      AUTOMATIC DRIVING CLASS
+                      <FaArrowRightLong className="w-4 h-4" />
+                    </Link>
+                  </li>
+                  <li className="flex rounded-md text-black hover:bg-gray-100 cursor-pointer">
+                    <Link
+                      href="/classes/3"
+                      className="flex items-center justify-between w-full px-2 py-2 whitespace-nowrap"
+                    >
+                      INTENSIVE DRIVING CLASS
+                      <FaArrowRightLong className="w-4 h-4" />
+                    </Link>
+                  </li>
+                  <li className="flex rounded-md text-black hover:bg-gray-100 cursor-pointer">
+                    <Link
+                      href="/classes/2"
+                      className="flex items-center justify-between w-full px-2 py-2 whitespace-nowrap"
+                    >
+                      BULK BOOKING CLASS
+                      <FaArrowRightLong className="w-4 h-4" />
+                    </Link>
+                  </li>
+                  <li className="flex rounded-md text-black hover:bg-gray-100 cursor-pointer">
+                    <Link
+                      href="/classes/3"
+                      className="flex items-center justify-between w-full px-2 py-2 whitespace-nowrap"
+                    >
+                      PRICING
+                      <FaArrowRightLong className="w-4 h-4" />
+                    </Link>
+                  </li>
+                </ul>
+              </HeaderDropdown>
+            </li>
+            <li>
+              <Link href="/">CONTACT US</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <div className="max-lg:hidden">
           <ButtonStyle1>Book Now</ButtonStyle1>
         </div>
+      </div>
+      {/* mobile navigation */}
+      <div
+        className={`lg:hidden flex flex-col items-center justify-center absolute top-0 left-0 rounded-b-4xl space-y-7 w-full pt-24 py-6 bg-linear-to-r from-[#A8A9AD] to-[#FFFFFF] ${
+          hamburgerDisplay ? "translate-y-0" : "-translate-y-full"
+        } transition-transform duration-300 ease-in-out`}
+      >
+        <ul className="flex flex-col gap-6 text-sm font-semibold">
+          <li className="text-center">
+            <Link href="/">ABOUT US</Link>
+          </li>
+          <li className="text-center">
+            <Link href="/">OUR TEAM</Link>
+          </li>
+          <li>
+            <HeaderDropdown
+              title="EXPLORE CLASSES"
+              titleHref="/explore-classes"
+            >
+              <ul className="px-2 py-2 text-xs font-medium">
+                <li className="flex items-center rounded-md text-black cursor-pointer">
+                  <Link
+                    href="/classes/1"
+                    className="flex items-center justify-between gap-4 w-full px-2 py-2 whitespace-nowrap"
+                  >
+                    MANUAL DRIVING CLASS
+                    <FaArrowRightLong className="w-4 h-4" />
+                  </Link>
+                </li>
+                <li className="flex rounded-md text-black cursor-pointer">
+                  <Link
+                    href="/classes/2"
+                    className="flex items-center justify-between gap-4 w-full px-2 py-2 whitespace-nowrap"
+                  >
+                    AUTOMATIC DRIVING CLASS
+                    <FaArrowRightLong className="w-4 h-4" />
+                  </Link>
+                </li>
+                <li className="flex rounded-md text-black cursor-pointer">
+                  <Link
+                    href="/classes/3"
+                    className="flex items-center justify-between gap-4 w-full px-2 py-2 whitespace-nowrap"
+                  >
+                    INTENSIVE DRIVING CLASS
+                    <FaArrowRightLong className="w-4 h-4" />
+                  </Link>
+                </li>
+                <li className="flex rounded-md text-black cursor-pointer">
+                  <Link
+                    href="/classes/2"
+                    className="flex items-center justify-between gap-4 w-full px-2 py-2 whitespace-nowrap"
+                  >
+                    BULK BOOKING CLASS
+                    <FaArrowRightLong className="w-4 h-4" />
+                  </Link>
+                </li>
+                <li className="flex rounded-md text-black cursor-pointer">
+                  <Link
+                    href="/classes/3"
+                    className="flex items-center justify-between gap-4 w-full px-2 py-2 whitespace-nowrap"
+                  >
+                    PRICING
+                    <FaArrowRightLong className="w-4 h-4" />
+                  </Link>
+                </li>
+              </ul>
+            </HeaderDropdown>
+          </li>
+          <li className="text-center">
+            <Link href="/">CONTACT US</Link>
+          </li>
+        </ul>
+
+        <ButtonStyle1>Book Now</ButtonStyle1>
       </div>
     </header>
   );
---
+++ components/HeaderDropdown.tsx
@@ -0,0 +1,84 @@
+"use client";
+import Link from "next/link";
+import { useEffect, useRef, ReactNode } from "react";
+import { FaCaretDown } from "react-icons/fa6";
+
+interface HeaderDropdownProps {
+  title: string;
+  titleHref?: string;
+  children: ReactNode;
+}
+
+const HeaderDropdown = ({
+  title,
+  titleHref,
+  children,
+}: HeaderDropdownProps) => {
+  const constainerRef = useRef<HTMLDivElement>(null);
+  const dropdownRef = useRef<HTMLDivElement>(null);
+
+  const handleClick = () => {
+    if (dropdownRef.current) {
+      dropdownRef.current.classList.toggle("hidden");
+    }
+  };
+
+  const handleOutsideClick = (event: MouseEvent) => {
+    if (
+      dropdownRef.current &&
+      !dropdownRef.current.contains(event.target as Node) &&
+      constainerRef.current &&
+      !constainerRef.current.contains(event.target as Node)
+    ) {
+      dropdownRef.current.classList.add("hidden");
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener("click", handleOutsideClick);
+    return () => {
+      document.removeEventListener("click", handleOutsideClick);
+    };
+  }, []);
+
+  return (
+    <div ref={constainerRef} className="relative z-10">
+      {titleHref ? (
+        <div className="w-full flex items-center justify-center gap-2 poppins-medium cursor-pointer">
+          <Link href={titleHref}>{title}</Link>
+          <FaCaretDown onClick={handleClick} />
+        </div>
+      ) : (
+        <button
+          className="w-full flex items-center justify-center gap-2 poppins-medium cursor-pointer"
+          onClick={handleClick}
+        >
+          {title}
+          <FaCaretDown />
+        </button>
+      )}
+      <div
+        ref={dropdownRef}
+        className="w-full lg:w-[160%] lg:absolute lg:left-1/2 lg:transform lg:-translate-x-1/2 hidden lg:bg-white rounded-xl lg:mt-4 lg:border border-[var(--custom-primary)]"
+      >
+        {/* speech bubble tails */}
+        {/* alternative => left-5/6 transform -translate-x-1/2 */}
+        <div className="max-lg:hidden absolute -top-4 right-8">
+          <div className="w-0 h-0 border-l-[16px] border-r-[16px] border-b-[16px] border-l-transparent border-r-transparent border-b-[var(--custom-primary)]"></div>
+          <div className="absolute top-[1px] left-[1px] w-0 h-0 border-l-[15px] border-r-[15px] border-b-[15px] border-l-transparent border-r-transparent border-b-white"></div>
+        </div>
+        {children}
+      </div>
+    </div>
+  );
+};
+
+export default HeaderDropdown;
+// Usage:
+// <Dropdown title="Menu">
+//   <ul className="p-4">
+//     <li className="py-2 hover:bg-gray-100">Item 1</li>
+//     <li className="py-2 hover:bg-gray-100">Item 2</li>
+//     <li className="py-2 hover:bg-gray-100">Item 3</li>
+//   </ul>
+// </Dropdown>
