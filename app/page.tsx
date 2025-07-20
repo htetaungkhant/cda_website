91abc330c53df91f1ce7cf6c9b26d40a5ed2526c
Htet Aung Khant <chitkogyi19950@gmail.com>
2025-07-21 01:36:23 +0700

    feat: enhance ReasonCardsSection with dynamic props for title, description, and card details

--- app/page.tsx
+++ app/page.tsx
@@ -18,7 +18,34 @@ export default function Home() {
       <CourseCardsSection />
 
       {/* Reason Cards Section */}
-      <ReasonCardsSection />
+      <ReasonCardsSection
+        title="Why Choose Us?"
+        description="At Cambridge Driving Academy, we go beyond lessons — we ensure a safe, flexible, and personalized driving experience that sets you up for success."
+        firstCard={{
+          title: "Certified, Friendly Instructors",
+          description:
+            "Our certified instructors build confidence and focus on safe driving habits for all skill levels.",
+          icon: "/5.svg",
+        }}
+        secondCard={{
+          title: "Personalised Learning Plans",
+          description:
+            "Personalized lessons are designed to meet your unique needs, whether you’re a beginner or experienced driver.",
+          icon: "/6.svg",
+        }}
+        thirdCard={{
+          title: "Flexible Scheduling",
+          description:
+            "Busy lifestyle? No problem. Choose your lesson times with ease and learn at a pace that suits you — mornings, evenings, or weekends.",
+          icon: "/7.svg",
+        }}
+        fourthCard={{
+          title: "Modern, Safe Vehicles",
+          description:
+            "Train in clean, well-maintained cars equipped with the latest safety tech. Your learning experience will be secure, smooth, and stress-free.",
+          icon: "/8.svg",
+        }}
+      />
 
       {/* Instructor Cards Section */}
       <InstructorCardsSection />
--- components/ReasonCardsSection.tsx
+++ components/ReasonCardsSection.tsx
@@ -1,99 +1,141 @@
-import Image from "next/image";
 import React from "react";
+import Image from "next/image";
+import { StaticImport } from "next/dist/shared/lib/get-img-props";
 
-import SvgImage5 from "@/public/5.svg";
-import SvgImage6 from "@/public/6.svg";
-import SvgImage7 from "@/public/7.svg";
-import SvgImage8 from "@/public/8.svg";
+interface ReasonCard {
+  icon?: string | StaticImport;
+  title?: string;
+  description?: string;
+}
 
-const ReasonCardsSection = () => {
+interface ReasonCardsSectionProps {
+  title: string;
+  description?: string;
+  firstCard?: ReasonCard;
+  secondCard?: ReasonCard;
+  thirdCard?: ReasonCard;
+  fourthCard?: ReasonCard;
+}
+
+const ReasonCardsSection: React.FC<ReasonCardsSectionProps> = ({
+  title,
+  description,
+  firstCard,
+  secondCard,
+  thirdCard,
+  fourthCard,
+}) => {
   return (
     <section className="max-w-420 mx-auto px-6 py-8 max-lg:px-3 max-lg:py-6 flex flex-col gap-4 text-black">
       <h1 className="text-4xl max-lg:text-center">
-        <strong>Why Choose Us?</strong>
+        <strong>{title}</strong>
       </h1>
-      <p className="max-lg:text-center">
-        At Cambridge Driving Academy, we go beyond lessons — we ensure a safe,
-        flexible, and personalized driving experience that sets you up for
-        success.
-      </p>
+      <p className="max-lg:text-center">{description}</p>
       <div className="grid grid-cols-2 lg:grid-cols-4 gap-2 md:gap-4">
         {/* Reason Card 1 */}
-        <div className="relative flex flex-col gap-2 md:gap-4 md:min-h-64 rounded-xl text-white bg-[url(/car-part-1.png)] bg-cover bg-no-repeat bg-center p-4 max-[330px]:p-2 xl:p-6 overflow-hidden">
-          {/* overlay */}
-          <div className="absolute inset-0 bg-black opacity-50" />
-          <Image
-            src={SvgImage5}
-            alt="Reason 1"
-            width={64}
-            height={64}
-            className="w-12 h-12 md:w-14 md:h-14 object-contain relative"
-          />
-          <h2 className="font-semibold text-lg md:text-xl relative">
-            Certified, Friendly Instructors
-          </h2>
-          <p className="text-sm md:text-base relative">
-            Our certified instructors build confidence and focus on safe driving
-            habits for all skill levels.
-          </p>
-        </div>
+        {firstCard && (
+          <div className="relative flex flex-col gap-2 md:gap-4 md:min-h-64 rounded-xl text-white bg-[url(/car-part-1.png)] bg-cover bg-no-repeat bg-center p-4 max-[330px]:p-2 xl:p-6 overflow-hidden">
+            {/* overlay */}
+            <div className="absolute inset-0 bg-black opacity-50" />
+            {firstCard.icon && (
+              <Image
+                src={firstCard.icon}
+                alt="Reason 1"
+                width={64}
+                height={64}
+                className="w-12 h-12 md:w-14 md:h-14 object-contain relative"
+              />
+            )}
+            {firstCard.title && (
+              <h2 className="font-semibold text-lg md:text-xl relative">
+                {firstCard.title}
+              </h2>
+            )}
+            {firstCard.description && (
+              <p className="text-sm md:text-base relative">
+                {firstCard.description}
+              </p>
+            )}
+          </div>
+        )}
         {/* Reason Card 2 */}
-        <div className="relative flex flex-col gap-2 md:gap-4 md:min-h-64 rounded-xl text-white bg-[url(/car-part-2.png)] bg-cover bg-no-repeat bg-center p-4 max-[330px]:p-2 xl:p-6 overflow-hidden">
-          {/* overlay */}
-          <div className="absolute inset-0 bg-black opacity-50" />
-          <Image
-            src={SvgImage6}
-            alt="Reason 2"
-            width={64}
-            height={64}
-            className="w-12 h-12 md:w-14 md:h-14 object-contain relative"
-          />
-          <h2 className="font-semibold text-lg md:text-xl relative">
-            Certified, Friendly Instructors
-          </h2>
-          <p className="text-sm md:text-base relative">
-            Our certified instructors build confidence and focus on safe driving
-            habits for all skill levels.
-          </p>
-        </div>
+        {secondCard && (
+          <div className="relative flex flex-col gap-2 md:gap-4 md:min-h-64 rounded-xl text-white bg-[url(/car-part-2.png)] bg-cover bg-no-repeat bg-center p-4 max-[330px]:p-2 xl:p-6 overflow-hidden">
+            {/* overlay */}
+            <div className="absolute inset-0 bg-black opacity-50" />
+            {secondCard.icon && (
+              <Image
+                src={secondCard.icon}
+                alt="Reason 2"
+                width={64}
+                height={64}
+                className="w-12 h-12 md:w-14 md:h-14 object-contain relative"
+              />
+            )}
+            {secondCard.title && (
+              <h2 className="font-semibold text-lg md:text-xl relative">
+                {secondCard.title}
+              </h2>
+            )}
+            {secondCard.description && (
+              <p className="text-sm md:text-base relative">
+                {secondCard.description}
+              </p>
+            )}
+          </div>
+        )}
         {/* Reason Card 3 */}
-        <div className="relative flex flex-col gap-2 md:gap-4 md:min-h-64 rounded-xl text-white bg-[url(/car-part-3.png)] bg-cover bg-no-repeat bg-center p-4 max-[330px]:p-2 xl:p-6 overflow-hidden">
-          {/* overlay */}
-          <div className="absolute inset-0 bg-black opacity-50" />
-          <Image
-            src={SvgImage7}
-            alt="Reason 3"
-            width={64}
-            height={64}
-            className="w-12 h-12 md:w-14 md:h-14 object-contain relative"
-          />
-          <h2 className="font-semibold text-lg md:text-xl relative">
-            Certified, Friendly Instructors
-          </h2>
-          <p className="text-sm md:text-base relative">
-            Our certified instructors build confidence and focus on safe driving
-            habits for all skill levels.
-          </p>
-        </div>
+        {thirdCard && (
+          <div className="relative flex flex-col gap-2 md:gap-4 md:min-h-64 rounded-xl text-white bg-[url(/car-part-3.png)] bg-cover bg-no-repeat bg-center p-4 max-[330px]:p-2 xl:p-6 overflow-hidden">
+            {/* overlay */}
+            <div className="absolute inset-0 bg-black opacity-50" />
+            {thirdCard.icon && (
+              <Image
+                src={thirdCard.icon}
+                alt="Reason 3"
+                width={64}
+                height={64}
+                className="w-12 h-12 md:w-14 md:h-14 object-contain relative"
+              />
+            )}
+            {thirdCard.title && (
+              <h2 className="font-semibold text-lg md:text-xl relative">
+                {thirdCard.title}
+              </h2>
+            )}
+            {thirdCard.description && (
+              <p className="text-sm md:text-base relative">
+                {thirdCard.description}
+              </p>
+            )}
+          </div>
+        )}
         {/* Reason Card 4 */}
-        <div className="relative flex flex-col gap-2 md:gap-4 md:min-h-64 rounded-xl text-white bg-[url(/car-part-4.png)] bg-cover bg-no-repeat bg-center p-4 max-[330px]:p-2 xl:p-6 overflow-hidden">
-          {/* overlay */}
-          <div className="absolute inset-0 bg-black opacity-50" />
-          <Image
-            src={SvgImage8}
-            alt="Reason 4"
-            width={64}
-            height={64}
-            className="w-12 h-12 md:w-14 md:h-14 object-contain relative"
-          />
-          <h2 className="font-semibold text-lg md:text-xl relative">
-            Certified, Friendly Instructors
-          </h2>
-          <p className="text-sm md:text-base relative">
-            Our certified instructors build confidence and focus on safe driving
-            habits for all skill levels.
-          </p>
-        </div>
+        {fourthCard && (
+          <div className="relative flex flex-col gap-2 md:gap-4 md:min-h-64 rounded-xl text-white bg-[url(/car-part-4.png)] bg-cover bg-no-repeat bg-center p-4 max-[330px]:p-2 xl:p-6 overflow-hidden">
+            {/* overlay */}
+            <div className="absolute inset-0 bg-black opacity-50" />
+            {fourthCard.icon && (
+              <Image
+                src={fourthCard.icon}
+                alt="Reason 4"
+                width={64}
+                height={64}
+                className="w-12 h-12 md:w-14 md:h-14 object-contain relative"
+              />
+            )}
+            {fourthCard.title && (
+              <h2 className="font-semibold text-lg md:text-xl relative">
+                {fourthCard.title}
+              </h2>
+            )}
+            {fourthCard.description && (
+              <p className="text-sm md:text-base relative">
+                {fourthCard.description}
+              </p>
+            )}
+          </div>
+        )}
       </div>
     </section>
   );
