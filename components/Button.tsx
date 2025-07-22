262fc8bdb7cefcf654d2124ec5d7d92c848c78a9
Htet Aung Khant <chitkogyi19950@gmail.com>
2025-07-22 16:00:53 +0700

    feat: enhance Button components with improved props handling and add WhiteArrow for better visual indication

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,22 +1,24 @@
 "use client";
-import React, { ButtonHTMLAttributes } from "react";
+import React, { AnchorHTMLAttributes, ButtonHTMLAttributes } from "react";
 import Image from "next/image";
 import Link from "next/link";
 import { motion } from "motion/react";
 
 import Arrow from "@/public/arrow.svg";
+import WhiteArrow from "@/public/arrow-white.svg";
 import { cn } from "@/lib/utils";
 
 export const ButtonStyle1: React.FC<
-  ButtonHTMLAttributes<HTMLButtonElement> & {
-    href?: string;
-  }
-> = ({ href, children, className, ...props }) => {
+  ButtonHTMLAttributes<HTMLButtonElement> &
+    AnchorHTMLAttributes<HTMLAnchorElement> & {
+      contentClassName?: string;
+    }
+> = ({ href, children, contentClassName, ...props }) => {
   const content = (
     <motion.div
       className={cn(
         "relative flex items-center gap-2 font-medium text-base p-1.5 pl-3.5 rounded-full bg-[var(--custom-primary)] cursor-pointer group overflow-hidden max-lg:p-1 max-lg:pl-3",
-        className
+        contentClassName
       )}
       whileHover="hover"
     >
@@ -63,11 +65,61 @@ export const ButtonStyle1: React.FC<
 
   if (href) {
     return (
-      <Link href={href} className="no-underline">
+      <Link href={href} {...props} className="no-underline">
         {content}
       </Link>
     );
   }
 
   return <button {...props}>{content}</button>;
+};
+
+export const ButtonStyle2: React.FC<
+  ButtonHTMLAttributes<HTMLButtonElement> &
+    AnchorHTMLAttributes<HTMLAnchorElement> & {
+      contentClassName?: string;
+    }
+> = ({ href, children, contentClassName, ...props }) => {
+  const content = (
+    <div
+      className={cn(
+        "relative flex items-center gap-2 font-medium text-base p-1.5 pl-3.5 rounded-full bg-white text-black cursor-pointer group overflow-hidden max-lg:p-1 max-lg:pl-3",
+        contentClassName
+      )}
+    >
+      {/* Content */}
+      <span className="relative z-10 whitespace-nowrap max-lg:text-sm">
+        {children}
+      </span>
+
+      {/* Arrow */}
+      <div className="relative z-10 bg-black rounded-full p-2 flex items-center justify-center w-8 h-8 max-lg:p-1.5 max-lg:w-7 max-lg:h-7 transition-all duration-500 ease-in-out group-hover:rotate-45">
+        <Image
+          src={WhiteArrow}
+          width={48}
+          height={48}
+          alt="Arrow"
+          className="w-full h-full object-contain"
+        />
+      </div>
+    </div>
+  );
+
+  if (href) {
+    return (
+      <Link
+        href={href}
+        {...props}
+        className={cn("no-underline group", props.className)}
+      >
+        {content}
+      </Link>
+    );
+  }
+
+  return (
+    <button {...props} className={cn("group", props.className)}>
+      {content}
+    </button>
+  );
 };
