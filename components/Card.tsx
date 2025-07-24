3d91872e842fbf9a8dba5b079172e774a0077648
Htet Aung Khant <chitkogyi19950@gmail.com>
2025-07-25 00:49:24 +0700

    fix: Update CardStyle1Props to make description optional and adjust rendering logic

--- components/Card.tsx
+++ components/Card.tsx
@@ -5,7 +5,7 @@ import { cn } from "@/lib/utils";
 
 interface CardStyle1Props {
   title: string;
-  description: string;
+  description?: string;
   iconUrl: string;
   className?: string;
 }
@@ -19,7 +19,7 @@ export function CardStyle1({
   return (
     <div
       className={cn(
-        "relative bg-gradient-to-b from-[#FFFDEE] to-[#F9F3B1] border border-[var(--custom-primary)] shadow-[4.17px_4.17px_12.5px_0px_rgba(0,0,0,0.25)] rounded-xl p-4 md:p-6 lg:p-8 xl:p-10 max-sm:min-h-44 flex flex-col justify-center items-center gap-2 text-center",
+        "relative text-[#2D2E2F] bg-gradient-to-b from-[#FFFDEE] to-[#F9F3B1] border border-[var(--custom-primary)] shadow-[4.17px_4.17px_12.5px_0px_rgba(0,0,0,0.25)] rounded-xl p-4 md:p-6 lg:p-8 xl:p-10 max-sm:min-h-44 flex flex-col justify-center items-center gap-2 text-center",
         className
       )}
     >
@@ -31,9 +31,11 @@ export function CardStyle1({
         className="w-12 h-12 xl:w-15 xl:h-15 object-contain"
       />
       <h2 className="text-xs sm:text-sm lg:text-base font-medium">{title}</h2>
-      <p className="overflow-hidden absolute inset-0 flex justify-center items-center bg-gradient-to-b from-[#FEF1D6] to-[#FDC24A] text-[#585858] rounded-xl p-1 sm:p-2 text-[10px] sm:text-xs lg:text-sm opacity-0 hover:opacity-100 transition-opacity duration-300 ease-in-out">
-        {description}
-      </p>
+      {description && (
+        <p className="overflow-hidden absolute inset-0 flex justify-center items-center bg-gradient-to-b from-[#FEF1D6] to-[#FDC24A] text-[#585858] rounded-xl p-1 sm:p-2 text-[10px] sm:text-xs lg:text-sm opacity-0 hover:opacity-100 transition-opacity duration-300 ease-in-out">
+          {description}
+        </p>
+      )}
     </div>
   );
 }
