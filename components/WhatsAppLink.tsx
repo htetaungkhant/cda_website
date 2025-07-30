dc623184141ae428340927fa78ac48ad7cb7af4c
Htet Aung Khant <chitkogyi19950@gmail.com>
2025-07-30 20:03:05 +0700

    fix: update WhatsApp link comment for clarity and adjust z-index for improved stacking

--- components/WhatsAppLink.tsx
+++ components/WhatsAppLink.tsx
@@ -5,10 +5,10 @@ import { FaWhatsapp } from "react-icons/fa";
 export function WhatsAppLink() {
   return (
     <Link
-      href="https://wa.me/447496842781" // +44 7496 842781
+      href="https://wa.me/447496842781" // +44 7496 842781 => https://api.whatsapp.com/send?phone=447496842781
       target="_blank"
       rel="noopener noreferrer"
-      className="fixed bottom-4 right-18 md:right-28 z-50 bg-[#60D669] text-white rounded-full shadow-lg hover:bg-green-500 transition-colors cursor-pointer"
+      className="fixed bottom-4 right-18 md:right-28 z-51 bg-[#60D669] text-white rounded-full shadow-lg hover:bg-green-500 transition-colors cursor-pointer"
     >
       <FaWhatsapp className="w-12 h-12 p-1 md:w-18 md:h-18" />
     </Link>
