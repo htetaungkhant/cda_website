a9e42a8d7a2307453c31c42238b9dc2f490fbd1e
Htet Aung Khant <chitkogyi19950@gmail.com>
2025-08-25 16:53:49 +0700

    feat: enhance instructor details page with dynamic data fetching and error handling

--- app/our-team/[instructorName]/page.tsx
+++ app/our-team/[instructorName]/page.tsx
@@ -1,9 +1,12 @@
 import React from "react";
 import Link from "next/link";
 import Image from "next/image";
+import { notFound } from "next/navigation";
+
 import { FaYoutube } from "react-icons/fa6";
 
 import TopUniformSection from "@/components/TopUniformSection";
+import { instructorService } from "@/services/server/instructor-service";
 import BookingForm from "../(components)/BookingForm";
 
 interface InstructorDetailsProps {
@@ -16,8 +19,17 @@ export default async function InstructorDetails({
   searchParams,
 }: InstructorDetailsProps) {
   const { instructorName } = await params;
-  const searchParamsObj = await searchParams;
-  console.log("Search Params:", searchParamsObj);
+  const { unique } = await searchParams;
+  if (!instructorName || !unique) {
+    notFound();
+  }
+
+  const instructor = await instructorService.getInstructorById(
+    unique as string
+  );
+  if (!instructor) {
+    notFound();
+  }
 
   return (
     <>
@@ -28,30 +40,21 @@ export default async function InstructorDetails({
           </>
         }
       >
-        <div className="mt-14 max-lg:mt-6 grid grid-cols-2 max-sm:grid-cols-1 bg-white shadow-md border border-[var(--custom-primary)] rounded-t-xl lg:rounded-t-2xl overflow-hidden">
+        <div className="mt-14 max-lg:mt-6 flex max-md:flex-col bg-white shadow-md border border-[var(--custom-primary)] rounded-t-xl lg:rounded-t-2xl overflow-hidden">
           <Image
-            src="/dummy-data/5.jpg"
-            alt={instructorName}
+            src={instructor?.image?.image}
+            alt={instructor?.name}
             width={650}
             height={650}
-            className="w-full h-full xl:max-h-108 object-cover"
+            className="w-full h-auto md:min-w-88 lg:min-w-108 object-cover"
+            priority
           />
           <div className="flex flex-col gap-6 max-lg:gap-2">
             <h2 className="px-1 sm:px-3 lg:px-6 py-2 sm:py-3 lg:py-4 text-xl lg:text-2xl font-semibold bg-gradient-to-r from-[#FFFAC6] to-[#FFFAC600] capitalize">
-              {instructorName.replace(/-/g, " ")}
+              {instructor?.name?.trim()}
             </h2>
             <p className="px-1 sm:px-3 lg:px-6 py-2 sm:py-3 lg:py-4 text-[#585858] text-sm md:text-base lg:text-lg">
-              Elnara Babayeva is a DVSA-approved automatic driving instructor
-              with a calm, friendly, and highly professional teaching style. She
-              brings patience and clarity to every lesson, making her especially
-              popular with beginners and nervous drivers. With a deep
-              understanding of learner needs, Elnara tailors each session to
-              help you progress at your own pace while focusing on safety,
-              confidence, and real-world driving skills. Her students
-              consistently praise her approachable nature and supportive
-              feedback. Whether you&apos;re just starting out or need a
-              confidence boost, Elnara is here to help you become a safe,
-              capable, and test-ready driver.
+              {instructor?.description}
             </p>
           </div>
         </div>
@@ -61,7 +64,7 @@ export default async function InstructorDetails({
             <div className="absolute inset-0 bg-black opacity-30"></div>
 
             <Link
-              href={`https://www.youtube.com/@${instructorName}`}
+              href={instructor?.link}
               target="_blank"
               rel="noopener noreferrer"
               className="relative z-10 flex items-center justify-center "
